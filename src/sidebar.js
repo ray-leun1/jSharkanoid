e1c96e9c7611bc6dc8232eaaafc0bcafd8dce6b3
ray-leun1 <ray.leun1@gmail.com>
2020-01-09 15:53:48 -0800

    Modify sidebar to use variables and relative positions instead of absolute values

--- src/sidebar.js
+++ src/sidebar.js
@@ -28,44 +28,44 @@ class Sidebar {
 
     if (aquarium.paused) {
       aquarium.ctx.beginPath();
-      aquarium.ctx.moveTo(550, 556);
-      aquarium.ctx.lineTo(573, 567);
-      aquarium.ctx.lineTo(550, 579);
+      aquarium.ctx.moveTo(Sidebar.LEFT + 100, 556);
+      aquarium.ctx.lineTo(Sidebar.LEFT + 123, 567);
+      aquarium.ctx.lineTo(Sidebar.LEFT + 100, 579);
       aquarium.ctx.closePath();
       aquarium.ctx.fill();
     } else {
       aquarium.ctx.beginPath();
-      aquarium.ctx.moveTo(550, 556);
-      aquarium.ctx.lineTo(560, 556);
-      aquarium.ctx.lineTo(560, 580);
-      aquarium.ctx.lineTo(550, 580);
+      aquarium.ctx.moveTo(Sidebar.LEFT + 100, 556);
+      aquarium.ctx.lineTo(Sidebar.LEFT + 110, 556);
+      aquarium.ctx.lineTo(Sidebar.LEFT + 110, 580);
+      aquarium.ctx.lineTo(Sidebar.LEFT + 100, 580);
       aquarium.ctx.closePath();
       aquarium.ctx.fill();
 
       aquarium.ctx.beginPath();
-      aquarium.ctx.moveTo(564, 556);
-      aquarium.ctx.lineTo(574, 556);
-      aquarium.ctx.lineTo(574, 580);
-      aquarium.ctx.lineTo(564, 580);
+      aquarium.ctx.moveTo(Sidebar.LEFT + 114, 556);
+      aquarium.ctx.lineTo(Sidebar.LEFT + 124, 556);
+      aquarium.ctx.lineTo(Sidebar.LEFT + 124, 580);
+      aquarium.ctx.lineTo(Sidebar.LEFT + 114, 580);
       aquarium.ctx.closePath();
       aquarium.ctx.fill();
 
       aquarium.ctx.beginPath();
-      aquarium.ctx.moveTo(550, 556);
-      aquarium.ctx.lineTo(574, 556);
-      aquarium.ctx.lineTo(574, 580);
-      aquarium.ctx.lineTo(550, 580);
+      aquarium.ctx.moveTo(Sidebar.LEFT + 100, 556);
+      aquarium.ctx.lineTo(Sidebar.LEFT + 124, 556);
+      aquarium.ctx.lineTo(Sidebar.LEFT + 124, 580);
+      aquarium.ctx.lineTo(Sidebar.LEFT + 100, 580);
       aquarium.ctx.closePath();
     }
 
     aquarium.ctx.beginPath();
-    aquarium.ctx.moveTo(474, 553);
-    aquarium.ctx.lineTo(577, 553);
-    aquarium.ctx.lineTo(577, 583);
-    aquarium.ctx.lineTo(547, 583);
-    aquarium.ctx.lineTo(547, 553);
-    aquarium.ctx.lineTo(547, 583);
-    aquarium.ctx.lineTo(474, 583);
+    aquarium.ctx.moveTo(Sidebar.LEFT + 24, 553);
+    aquarium.ctx.lineTo(Sidebar.LEFT + 127, 553);
+    aquarium.ctx.lineTo(Sidebar.LEFT + 127, 583);
+    aquarium.ctx.lineTo(Sidebar.LEFT + 97, 583);
+    aquarium.ctx.lineTo(Sidebar.LEFT + 97, 553);
+    aquarium.ctx.lineTo(Sidebar.LEFT + 97, 583);
+    aquarium.ctx.lineTo(Sidebar.LEFT + 24, 583);
     aquarium.ctx.closePath();
     aquarium.ctx.strokeStyle = '#064273';
     aquarium.ctx.lineWidth = 2;
@@ -75,48 +75,51 @@ class Sidebar {
   draw(aquarium) {
     aquarium.ctx.fillStyle = 'rgba(127,205,255, 0.5)';
     aquarium.ctx.beginPath();
-    aquarium.ctx.moveTo(this.Aquarium.WIDTH, 0);
-    aquarium.ctx.lineTo(this.Aquarium.CANVAS_WIDTH, 0);
-    aquarium.ctx.lineTo(this.Aquarium.CANVAS_WIDTH, this.Aquarium.CANVAS_HEIGHT);
-    aquarium.ctx.lineTo(this.Aquarium.WIDTH, this.Aquarium.CANVAS_HEIGHT);
+    aquarium.ctx.moveTo(Sidebar.LEFT, 0);
+    aquarium.ctx.lineTo(Sidebar.LEFT + Sidebar.WIDTH, 0);
+    aquarium.ctx.lineTo(Sidebar.LEFT + Sidebar.WIDTH, this.Aquarium.CANVAS_HEIGHT);
+    aquarium.ctx.lineTo(Sidebar.LEFT, this.Aquarium.CANVAS_HEIGHT);
     aquarium.ctx.closePath();
     aquarium.ctx.fill();
 
     aquarium.ctx.fillStyle = '#064273';
     aquarium.ctx.font = 'bold 19px sans-serif';
     aquarium.ctx.textAlign = 'center';
-    aquarium.ctx.fillText('jSharkanoid', (this.Aquarium.CANVAS_WIDTH + this.Aquarium.WIDTH) / 2, 30)
+    aquarium.ctx.fillText('jSharkanoid', Sidebar.WIDTH / 2, 30)
     aquarium.ctx.font = '18px monospace';
     aquarium.ctx.textAlign = 'left';
-    aquarium.ctx.fillText(aquarium.nommed.smol + 'x', 460, 70);
-    aquarium.ctx.fillText(aquarium.nommed.medium + 'x', 460, 110);
-    aquarium.ctx.fillText(aquarium.nommed.large + 'x', 460, 150);
-    aquarium.ctx.fillText(aquarium.nommed.huge + 'x', 460, 190);
-    aquarium.ctx.fillText(aquarium.nommed.duck + 'x', 460, 230);
-    aquarium.ctx.drawImage(this.assets.smol, 500, 62);
-    aquarium.ctx.drawImage(this.assets.medium, 500, 97);
-    aquarium.ctx.drawImage(this.assets.large, 500, 132, 0.9 * 30, 0.9 * 30);
-    aquarium.ctx.drawImage(this.assets.huge, 500, 175, 0.8 * 62, 0.8 * 32);
-    aquarium.ctx.drawImage(this.assets.duck, 500, 215, 0.7 * 128, 0.7 * 29);
+    aquarium.ctx.fillText(aquarium.nommed.smol + 'x', Sidebar.LEFT + 10, 70);
+    aquarium.ctx.fillText(aquarium.nommed.medium + 'x', Sidebar.LEFT + 10, 110);
+    aquarium.ctx.fillText(aquarium.nommed.large + 'x', Sidebar.LEFT + 10, 150);
+    aquarium.ctx.fillText(aquarium.nommed.huge + 'x', Sidebar.LEFT + 10, 190);
+    aquarium.ctx.fillText(aquarium.nommed.duck + 'x', Sidebar.LEFT + 10, 230);
+    aquarium.ctx.drawImage(this.assets.smol, Sidebar.LEFT + 50, 62);
+    aquarium.ctx.drawImage(this.assets.medium, Sidebar.LEFT + 50, 97);
+    aquarium.ctx.drawImage(this.assets.large, Sidebar.LEFT + 50, 132, 0.9 * 30, 0.9 * 30);
+    aquarium.ctx.drawImage(this.assets.huge, Sidebar.LEFT + 50, 175, 0.8 * 62, 0.8 * 32);
+    aquarium.ctx.drawImage(this.assets.duck, Sidebar.LEFT + 50, 215, 0.7 * 128, 0.7 * 29);
     aquarium.ctx.textAlign = 'center'
 
-    aquarium.ctx.fillText('Shark Speed', (this.Aquarium.CANVAS_WIDTH + this.Aquarium.WIDTH) / 2, 280);
-    aquarium.ctx.fillText(aquarium.calcSharkSpeed().toFixed(1), (this.Aquarium.CANVAS_WIDTH + this.Aquarium.WIDTH) / 2, 310);
+    aquarium.ctx.fillText('Shark Speed', Sidebar.WIDTH / 2, 280);
+    aquarium.ctx.fillText(aquarium.calcSharkSpeed().toFixed(1), Sidebar.WIDTH / 2, 310);
 
-    aquarium.ctx.fillText('Shark Lives', (this.Aquarium.CANVAS_WIDTH + this.Aquarium.WIDTH) / 2, 350);
+    aquarium.ctx.fillText('Shark Lives', Sidebar.WIDTH / 2, 350);
     if (aquarium.shark.hp < 1) {
       aquarium.ctx.drawImage(this.assets.death, 476, 365);
     } else {
-      if (aquarium.shark.hp >= 1) aquarium.ctx.drawImage(this.assets.life, 465, 365, 36, 36);
-      if (aquarium.shark.hp >= 2) aquarium.ctx.drawImage(this.assets.life, 506, 365, 36, 36);
-      if (aquarium.shark.hp >= 3) aquarium.ctx.drawImage(this.assets.life, 547, 365, 36, 36);
-      if (aquarium.shark.hp >= 4) aquarium.ctx.drawImage(this.assets.life, 465, 405, 36, 36);
-      if (aquarium.shark.hp >= 5) aquarium.ctx.drawImage(this.assets.life, 506, 405, 36, 36);
-      if (aquarium.shark.hp >= 6) aquarium.ctx.drawImage(this.assets.life, 547, 405, 36, 36);
+      if (aquarium.shark.hp >= 1) aquarium.ctx.drawImage(this.assets.life, Sidebar.LEFT + 15, 365, 36, 36);
+      if (aquarium.shark.hp >= 2) aquarium.ctx.drawImage(this.assets.life, Sidebar.LEFT + 56, 365, 36, 36);
+      if (aquarium.shark.hp >= 3) aquarium.ctx.drawImage(this.assets.life, Sidebar.LEFT + 97, 365, 36, 36);
+      if (aquarium.shark.hp >= 4) aquarium.ctx.drawImage(this.assets.life, Sidebar.LEFT + 15, 405, 36, 36);
+      if (aquarium.shark.hp >= 5) aquarium.ctx.drawImage(this.assets.life, Sidebar.LEFT + 56, 405, 36, 36);
+      if (aquarium.shark.hp >= 6) aquarium.ctx.drawImage(this.assets.life, Sidebar.LEFT + 97, 405, 36, 36);
     }
 
     this.drawBtns(aquarium);
   }
 }
+
+Sidebar.LEFT = this.Aquarium.WIDTH;
+Sidebar.WIDTH = 150;
 
 module.exports = Sidebar;
